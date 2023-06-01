e1ea392f43aa3dae4be10c03da6b090c7f2d6024
Hamaz Abdelghani <101653284+eljefe213@users.noreply.github.com>
2023-06-01 23:15:28 +0200

    use prettyChatWindow

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -4,8 +4,10 @@ const AuthPage = (props) => {
   const onSubmit = (e) => {
     e.preventDefault();
     const { value } = e.target[0];
-    axios.post("http://localhost:3001/authenticate");
-    props.onAuth({ username: value, secret: value });
+    axios
+      .post("http://localhost:3001/authenticate", { username: value })
+      .then((r) => props.onAuth({ ...r.data, secret: value }))
+      .catch((e) => console.log("error", e));
   };
 
   return (
--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,15 @@
-const ChatsPage = () => {
-  return <>chats...</>;
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh" }}>
+      <PrettyChatWindow
+        projectId="756d1911-11e8-414e-99d5-a99d28835ad5"
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
 };
 
 export default ChatsPage;
